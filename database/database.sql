f5d5e1f237d30ccde3453f2f11291c8a666d4778
Jéssica Leal <62772815+Jessy777-cripto@users.noreply.github.com>
2021-07-02 11:23:43 -0300

    almost there 2
    part of the database triggers was inserted

--- database/database.sql
+++ database/database.sql
@@ -1,6 +1,6 @@
 begin;
-create schema Compania_aereaTeste2;
-use Compania_aereaTeste2;
+create schema Compania_aereaPop;
+use Compania_aereaPop;
 
 create table Aeroporto(
     Codigo_aeroporto INT auto_increment,
@@ -24,13 +24,13 @@ create table Trecho_voo(
     Horario_partida_previsto varchar(5) null, 
     Codigo_aeroporto_chegada int unique not null, 
     Horario_chegada_previsto varchar(10) null,
-    PRIMARY KEY(Numero_voo, Numero_trecho)
+    PRIMARY KEY(Numero_trecho)
 )ENGINE=InnoDB;
 
 create table TARIFA (
     Numero_voo int,
     Codigo_tarifa INT,
-    Quantidade decimal(4,2) null,
+    Quantidade decimal(6,2) null,
     Restricoes VARCHAR (50) null,
     PRIMARY KEY (Numero_voo,Codigo_tarifa)
 )ENGINE=InnoDB;
@@ -58,31 +58,29 @@ create table AERONAVE (
 create table RESERVA_ASSENTO(
     Numero_voo INT,
     Numero_trecho INT,
-    Data DATE,
+    Data1 DATE,
     Numero_assento VARCHAR(10),
     Nome_cliente VARCHAR(50) null,
     Telefone_cliente VARCHAR(10) unique,
-    PRIMARY KEY(Numero_voo, Numero_trecho, Data, Numero_assento)
+    PRIMARY KEY(Numero_voo, Numero_trecho, Data1, Numero_assento)
 )ENGINE=InnoDB;
 
 create table Instancia_trecho(
     Numero_voo INT, 
     Numero_trecho int, 
-    Data date, 
+    Data1 date, 
     Numero_assentos_disponiveis int null, 
     Codigo_aeronave int unique null, 
     Codigo_aeroporto_partida int unique null, 
     Horario_partida varchar(10)  null, 
     Codigo_aeroporto_chegada int unique null, 
     Horario_chegada varchar(10) null,
-    primary key (Numero_voo, Numero_trecho, Data)
-    -- constraint foreign key fk_ins_ar (Codigo_aeroporto_partida) references Aeroporto(Codigo_aeroporto),
-    -- constraint foreign key fk_ins_ar2 (Codigo_aeroporto_chegada) references Aeroporto(Codigo_aeroporto),
-    -- constraint foreign key fk_ins_coar (Codigo_aeronave) references AERONAVE (Codigo_aeronave),
-    -- constraint foreign key fk_ins_ntr (Numero_trecho) references Trecho_voo(Numero_trecho)
+    primary key (Numero_voo, Numero_trecho, Data1),    
+    constraint Codigo_aeroporto_partida_1 foreign key fk_ins_ar (Codigo_aeroporto_partida) references Aeroporto (Codigo_aeroporto),
+    constraint Codigo_aeroporto_chegada_1 foreign key  fk_ins_ar2 (Codigo_aeroporto_chegada) references Aeroporto (Codigo_aeroporto),
+    constraint Numero_trecho_1 foreign key fk_ins_ntr (Numero_trecho) references  Trecho_voo(Numero_trecho),
+    constraint Codigo_aeronave_1 foreign key fk_ins_coar (Codigo_aeronave) references  AERONAVE (Codigo_aeronave)
 )ENGINE=InnoDB;
-
-
 -- foreign keys section
 alter table Trecho_voo
 -- all fk inserted
@@ -114,8 +112,25 @@ alter table Instancia_trecho
 add foreign key (Codigo_aeroporto_partida )references Aeroporto(Codigo_aeroporto) ON UPDATE CASCADE ON DELETE CASCADE,
 add foreign key (Codigo_aeroporto_chegada) references Aeroporto(Codigo_aeroporto) ON UPDATE CASCADE ON DELETE CASCADE,
 -- AERONAVE table
-add foreign key (Codigo_aeronave) references AERONAVE (Codigo_aeronave) ON UPDATE CASCADE ON DELETE CASCADE
--- Trecho_voo table, error reference
-;
+add foreign key (Codigo_aeronave) references AERONAVE (Codigo_aeronave) ON UPDATE CASCADE ON DELETE CASCADE,
+-- Trecho_voo table, error line
+add foreign key (Numero_trecho) references Trecho_voo (Numero_trecho) ON UPDATE CASCADE ON DELETE CASCADE;
 -- RESERVA_ASSENTO table 
+
+--end of the foreign keys section
+
+-- trigger section
+create trigger tr_desconto before insert
+on TARIFA
+for each row
+set new.Quantidade = (new.Quantidade*0.85);
+
+create trigger tr_nassento after insert
+on reserva_assento
+for each row
+update Instancia_trecho
+set Numero_assentos_disponiveis = Numero_assentos_disponiveis - 1
+where (Instancia_trecho.Numero_voo = RESERVA_ASSENTO.Numero_voo )and (Instancia_trecho.Numero_trecho = RESERVA_ASSENTO.Numero_trecho) and Instancia_trecho.Data1 = RESERVA_ASSENTO.Data1;
+
+
 commit;
